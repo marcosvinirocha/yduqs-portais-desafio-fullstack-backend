40e5108fec5f0e71baea8017321e6e844feaf31d
Marcos Vinicius Oliveira Rocha <marcosvinicius.udia1256@gmail.com>
2025-11-02 15:55:25 -0300

    feat:(bakend) ajust cors to receive data from frontend

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: false,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('yduqs-portais-desafio-fullstack')
     .setDescription('Documentação dos endpoints feito em Nest.js')
@@ -16,6 +22,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
